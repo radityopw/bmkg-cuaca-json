13129210ca1269dee1d71b575f354f548b5528bc
radityopw <radityo_pw@is.its.ac.id>
2021-11-23 15:09:14 +0000

    generate file json cuaca

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-main',
-    'version' => 'dev-main',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fc1e000b79751b622b3204adb26547e04a24eec2',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => '31291c52b3da9fb3bff8faf792861e95fcbc738d',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '31291c52b3da9fb3bff8faf792861e95fcbc738d',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fc1e000b79751b622b3204adb26547e04a24eec2',
+            'dev_requirement' => false,
+        ),
+        'simplehtmldom/simplehtmldom' => array(
+            'pretty_version' => '2.0-RC2',
+            'version' => '2.0.0.0-RC2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../simplehtmldom/simplehtmldom',
+            'aliases' => array(),
+            'reference' => '3c87726400e59d8e1bc4709cfe82353abeb0f4d1',
+            'dev_requirement' => false,
+        ),
     ),
-    'simplehtmldom/simplehtmldom' => 
-    array (
-      'pretty_version' => '2.0-RC2',
-      'version' => '2.0.0.0-RC2',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '3c87726400e59d8e1bc4709cfe82353abeb0f4d1',
-    ),
-  ),
 );
